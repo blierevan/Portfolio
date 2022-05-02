a359e7e2753ea8be25bc172924a1d38e201ccf7f
unknown <blier.evan@gmail.com>
2022-05-02 17:50:59 +0200

    50

--- veille.php
+++ veille.php
@@ -111,19 +111,18 @@
             </div>
         </div>
     </section>
-    <!-- Footer-->
-    <footer class="footer py-4">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2022</div>
-                <div class="col-lg-4 my-3 my-lg-0">
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="LinkedIn"><i class="fab fa-linkedin-in"></i></a>
+    <footer class="footer py-4 ">
+        <div class="container ">
+            <div class="row align-items-center ">
+                <div class="col-lg-4 text-lg-start ">Copyright &copy; Template Bootstrap 2022</div>
+                <div class="col-lg-4 my-3 my-lg-0 ">
+                    <a class="btn btn-dark btn-social mx-2 " href="mailto:blier.evan@gmail.com" aria-label="Email"><i class="fa fa-envelope "></i></a>
+                    <a class="btn btn-dark btn-social mx-2 " href="https://github.com/blierevan" aria-label="Github"><i class="fab fa-github "></i></a>
+                    <a class="btn btn-dark btn-social mx-2 " href="https://www.linkedin.com/in/evan-blier-07843822b/" aria-label="LinkedIn "><i class="fab fa-linkedin-in "></i></a>
+                    <a class="btn btn-dark btn-social mx-2 " href="tel:+33640081509" aria-label="téléphone"><i class="fa fa-phone "></i></a>
                 </div>
-                <div class="col-lg-4 text-lg-end">
-                    <a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
-                    <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>
+                <div class="col-lg-4 text-lg-end ">
+                    <a class="link-dark text-decoration-none me-3 " href="infolegale.html">Info légale</a>
                 </div>
             </div>
         </div>
