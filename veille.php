2a90d986400280d9dd1b04d256d2ea7fbdbf828a
BLIER Evan <blieeva@ndlp.fr>
2022-05-03 12:53:15 +0200

    veilleé

--- veille.php
+++ veille.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Portfolio</title>
+    <title>Portfolio Evan Blier</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     <!-- Font Awesome icons (free version)-->
@@ -38,14 +38,87 @@
             </div>
         </div>
     </nav>
-    <!-- Masthead-->
+    <section class="page-section bg-light" id="apropos">
+        <div class="container">
+            <div class="text-center">
+            <br><br><br>
+                <h2 class="section-heading text-uppercase">WEB 3.0</h2>
+                <br>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <p class="large ">Le Web 3 ou le Web 3.0 est la nouvelle génération d’internet. Le contrôle du web est remis entre les mains des utilisateurs. 
+                        La distinction est créée par des technologies émergentes telles que la blockchain.
+                         Lorsque nous communiquons via Internet, des copies de nos données sont créées et conservées sur 
+                        les serveurs d'entreprises telles que Google ou Facebook, et nous perdons ainsi le contrôle de nos données. 
+                        Le fait que nos informations soient détenues par des tiers n'est pas en soi une chose négative ; néanmoins, 
+                        lorsqu'une seule entreprise assure la médiation de l'ensemble du processus, les choses peuvent mal tourner.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="page-section bg-light" id="apropos">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="text-center">
+                    <br><br><br><br>
+                    <h3 class="section-heading text-uppercase">Source Google Alerts</h3>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-10 mx-auto">
+                            <div class="row py-5">
+                                <div class="col-lg-4">
+                                    <figure class="rounded p-3 bg-white shadow-sm">
+                                        <div class="img-fluid2">
+                                            <img src="assets/img/portfolio/web3.png" alt="" class="w-100 card-img-bottom">
+                                        </div>
+                                        <figcaption class="p-4 card-img-bottom">
+                                            <h5 class="h5 font-weight-bold mb-2 font-italic">Le Web 3.0, un outil de la parfaite transparence des informations ?</h5><br>
+                                            <a class="btn btn-primary btn-xl text-uppercase" target="_blank" href="https://www.zonebourse.com/actualite-bourse/Episode-12-Le-Web-3-0-un-outil-de-la-parfaite-transparence-des-informations---40247475/">Article</a>
+
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                                <div class="col-lg-4">
+                                    <figure class="rounded p-3 bg-white shadow-sm">
+                                        <div class="img-fluid2">
+                                            <img src="assets/img/portfolio/meta.png" alt="" class="w-100 card-img-top">
+                                        </div>
+                                        <figcaption class="p-4 card-img-bottom">
+                                            <h5 class="h5 font-weight-bold mb-2 font-italic">Construire un écosystème multichaîne pour accélérer la vision du métaverse</h5>
+                                            <a class="btn btn-primary btn-xl text-uppercase" target="_blank" href="https://fr.techtribune.net/web3/construire-un-ecosysteme-multichaine-pour-accelerer-la-vision-du-metaverse/306480/">Article</a>
+
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                                <div class="col-lg-4">
+                                    <figure class="rounded p-3 bg-white shadow-sm">
+                                        <div class="img-fluid2">
+                                            <img src="assets/img/portfolio/web3-.png" alt="" class="w-100 card-img-top">
+                                        </div>
+                                        <figcaption class="p-4 card-img-bottom">
+                                            <h5 class="h5 font-weight-bold mb-2 font-italic">Web3 : révolution ou illusion ?</h5><br><br>
+                                            <a class="btn btn-primary btn-xl text-uppercase" target="_blank" href="https://bigmedia.bpifrance.fr/news/web3-revolution-ou-illusion">Article</a>
+
+                                        </figcaption>
+                                    </figure>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <section class="page-section bg-light" id="apropos">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                 <?php
-                                echo "<h1 style='text-align:center;'>FLUX RSS</h1>";
-                                echo "<h1 style='padding:0px 20px;'>Journal du coin</h1>";
+                                echo "<h3>Source par FLUX RSS</h3><br>";
+                                echo "<h4 style='padding:0px 20px;'>Journal du coin</h4>";
                                 $url = "https://journalducoin.com/tag/web-3/feed/"; 
                                 $rss = simplexml_load_file($url);
                                 echo '<ul>';
@@ -57,7 +130,7 @@
                                 echo '</ul>';
 
 
-                                echo "<h1 style='padding:0px 20px;'>We Demain</h1>";
+                                echo "<h4 style='padding:0px 20px;'>We Demain</h4>";
                                 $url2 = "https://www.wedemain.fr/dechiffrer/metaverse-de-quoi-sagit-il-et-faut-il-sy-interesser/feed/"; 
                                 $rss = simplexml_load_file($url2);
                                 echo '<ul>';
